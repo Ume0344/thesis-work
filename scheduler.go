88b4fbcaba929a2e19ddef1dd76fc589ac5e96ef
Um e Habiba <uhabiba005@gmail.com>
2023-10-26 12:33:47 +0200

    Update scheduler.go to exclude master node for scheduling

--- scheduler.go
+++ scheduler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"math/rand"
 
 	v1 "k8s.io/api/core/v1"
@@ -11,9 +12,31 @@ import (
 
 // Scheduling UseCase1
 func findRandomNode(k8sclient kubernetes.Clientset) v1.Node {
+	var workerNodeList []v1.Node
+	var masterFlag bool
+
 	nodeList, _ := k8sclient.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+
+	for _, node := range nodeList.Items {
+		for label := range node.Labels {
+			masterFlag = false
+			if label == "node-role.kubernetes.io/control-plane" {
+				masterFlag = true
+				break
+			}
+		}
+		if !masterFlag {
+			workerNodeList = append(workerNodeList, node)
+		}
+	}
+
+	log.Println("Printing avaialable worker nodes")
+	for i, node := range workerNodeList {
+		log.Printf("worker node %d: %s", i+1, node.Name)
+	}
+
 	// Finding a random node to schedule p4 resource
-	selectedNode := nodeList.Items[rand.Intn(len(nodeList.Items))]
+	selectedNode := workerNodeList[rand.Intn(len(workerNodeList))]
 
 	return selectedNode
 }
