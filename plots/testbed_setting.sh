3194afedc21e0ee2f6793ca51877f8ef7bbfd642
Um e Habiba <uhabiba005@gmail.com>
2023-11-21 11:26:58 +0100

    Update testbed_setting to randomize setting

--- plots/testbed_setting.sh
+++ plots/testbed_setting.sh
@@ -5,10 +5,18 @@
 MANIFEST_FOLDER=/home/master/p4kube/manifests
 
 NETWORK_FUNCTION=$1
-YAML=("$NETWORK_FUNCTION-p42c.yaml" "$NETWORK_FUNCTION-p42c1.yaml" "$NETWORK_FUNCTION-c-compile.yaml")
+YAML=("$NETWORK_FUNCTION-p42c.yaml" "$NETWORK_FUNCTION-c-compile.yaml")
 
-for file in "${YAML[@]}";
+for ((i=0; i<3; i++));
 do
+  random_number=$((RANDOM % 3))
+  echo $random_number
+  if [ $random_number == 2 ];then
+    continue
+  fi
+  
+  file=${YAML["$random_number"]}
+  echo "Manifest file to be deployed: $file"
   kubectl create -f $MANIFEST_FOLDER/$NETWORK_FUNCTION/$file
-  sleep 3
+  sleep 5
 done
